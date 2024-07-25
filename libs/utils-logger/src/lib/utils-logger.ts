3635bf9f7d9dd3dc8c10660a9b250dcd3fd61583
Marcin Krawczyk <marckraw@icloud.com>
2024-07-25 15:57:23 +0200

    fix(utils-logger): remove unnecesary logs

--- libs/utils-logger/src/lib/utils-logger.ts
+++ libs/utils-logger/src/lib/utils-logger.ts
@@ -1,6 +1,5 @@
 import consola from 'consola';
 
 export const createLogger = (context: string) => {
-  console.log("Just console log in logger.")
   return consola.create({ level: 5, defaults: { tag: context } });
 };

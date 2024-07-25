1675dd0c2d736f158e5668e6138e1120c76de132
Marcin Krawczyk <marckraw@icloud.com>
2024-07-25 14:19:00 +0200

    fix(anton-sdk): bring back setting up axios for browser if browser envirpnment

--- libs/anton-sdk/src/lib/anton-sdk.ts
+++ libs/anton-sdk/src/lib/anton-sdk.ts
@@ -1,14 +1,14 @@
 import { ModelFactory, ModelType } from "./models/factory";
 import { AIModel, Message } from "./models/base";
-// import { setupAxios } from "./helpers/browser";
+import { setupAxios } from "./helpers/browser";
 import { createLogger } from '@anton-suite/utils-logger';
 
 const logger = createLogger('AntonSDK');
 
 // Run setup if in browser environment
-// if (typeof window !== "undefined") {
-//   setupAxios();
-// }
+if (typeof window !== "undefined") {
+  setupAxios();
+}
 export class AntonSDK {
   private model: AIModel;
   constructor(config: { type: ModelType; apiKey: string }) {
